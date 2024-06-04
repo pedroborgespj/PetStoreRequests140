7a9a10cbc5d15ed0ac64b4c2b899d6aace96aa5f
Pedro Borges <pedroborgespj@gmail.com>
2024-06-03 21:20:07 -0300

    implementando test_user_post & test_user_delete utilizando json dinamico

--- __tests__/test_user.py
+++ __tests__/test_user.py
@@ -3,6 +3,8 @@ import pytest    # framework de teste de unidade / engine
 import requests  # framework de teste de API
 import json
 
+from utils.utils import ler_csv
+
 # 2 - clase (opcional no Python, em muitos casos)
 
 # 2.1 - Atributos ou variaveis
@@ -87,3 +89,51 @@ def test_delete_user():
     assert response_body['code'] == 200
     assert response_body['type'] == 'unknown'
     assert response_body['message'] == user_username
+
+@pytest.mark.parametrize('user_id, username, first_name, last_name, user_email, user_password, user_phone, user_status',
+                         ler_csv('./fixtures/csv/users.csv') 
+                        )
+
+def test_post_user_dinamico(user_id, username, first_name, last_name, user_email, user_password, user_phone, user_status):
+    user = {}
+    user['id'] = int(user_id)
+    user['username'] = username
+    user['firstName'] = first_name
+    user['lastName'] = last_name
+    user['email'] = user_email
+    user ['password'] = user_password
+    user['phone'] = user_phone
+    user['userStatus'] = int(user_status)
+    
+    user = json.dumps(obj=user, indent=4)
+
+    response = requests.post(
+        url=url,
+        headers=headers,
+        data=user,
+        timeout=5
+    )
+
+    response_body = response.json()
+
+    assert response_body['code'] == 200
+    assert response_body['type'] == 'unknown'
+    assert response_body['message'] == user_id
+
+@pytest.mark.parametrize('username', 
+                         [row[1] for row in ler_csv('./fixtures/csv/users.csv')]
+                        )
+
+def test_delete_user_dinamico(username):
+    response = requests.delete(
+        url= f'{url}/{username}',
+        headers= headers
+        # nao tem corpo da mensagem / body
+    )
+
+    response_body = response.json()
+
+    assert response.status_code == 200
+    assert response_body['code'] == 200
+    assert response_body['type'] == 'unknown'
+    assert response_body['message'] == username
